d35c1011db2151de2c82703bfd79ab60dc222ab5
oliver <oliver.renwick@gmail.com>
2019-03-30 11:57:48 -0400

    Routing to basic content on React pages

--- ui/client/src/components/pages/login.js
+++ ui/client/src/components/pages/login.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
+import "../components.css"
     
 
 class Login extends Component {
     render() {
         return (
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <div class="input-group">
-                        <span class="input-group-addon" id="basic-addon1">username</span>
-                        <input type="text" class="form-control" placeholder="username" aria-describedby="basic-addon1"></input>
+            <div>
+
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Log In</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Log in to see your leases</h6>
+                    <p className="card-text">By logging in, you will be able to see the leases you have executed through OpenLease, and download .pdf versions of them.</p>
+                    <div className="input-group">
+                        <span className="input-group-addon" id="basic-addon1">username</span>
+                        <input type="text" className="form-control" placeholder="username" aria-describedby="basic-addon1"></input>
                     </div>
-                    <div class="input-group">
-                        <span class="input-group-addon" id="basic-addon1">password</span>
-                        <input type="text" class="form-control" placeholder="password" aria-describedby="basic-addon1"></input>
+                    <div className="input-group">
+                        <span className="input-group-addon" id="basic-addon1">password</span>
+                        <input type="text" className="form-control" placeholder="password" aria-describedby="basic-addon1"></input>
                     </div>
                 </div>
+            </div>
+
             </div>
         );
     }
