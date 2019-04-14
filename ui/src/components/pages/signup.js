1ce3eb5c0d4196b7a2ba1d2b4b792a8062c97b37
oliver <oliver.renwick@gmail.com>
2019-04-13 23:05:56 -0400

    modified signup page

--- ui/src/components/pages/signup.js
+++ ui/src/components/pages/signup.js
@@ -11,29 +11,13 @@ class Signup extends Component {
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Create a new account</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Enter your information below to create an account.</h6>
-                    <p className="card-text">OpenLease takes privacy very seriously. Your information is yours: that's one of the main motivations
-                    behind the creation of this project. So we will never use your information for any purpose other than allowing you to log in
-                    and access your leases.</p>
+                    <h6 className="card-subtitle mb-2 text-muted">Sign up before making a lease</h6>
+                    <p className="card-text">In order to user OpenLease's services, you'll need an account with OpenLaw. Don't worry--it's quick and free.
                     <br></br>
-                    <div className="input-group">
-                        <span className="input-group-addon" id="basic-addon1">username :</span>
-                        <input type="text" className="form-control, entry" placeholder="username" aria-describedby="basic-addon1"></input>
-                    </div>
+                    Follow the link here to sign up, and come back when you've created an account. This will allow OpenLaw to handle the signing of your lease.
                     <br></br>
-                    <div className="input-group">
-                        <span className="input-group-addon" id="basic-addon1">password :</span>
-                        <input type="text" className="form-control, entry" placeholder="password" aria-describedby="basic-addon1"></input>
-                    </div>
-                    <br></br>
-                    <div className="input-group">
-                        <span className="input-group-addon" id="basic-addon1">email :</span>
-                        <input type="text" className="form-control, entry" placeholder="email" aria-describedby="basic-addon1"></input>
-                    </div>
-
-                    <br></br>
-
-                    <button type="submit" className="btn btn-primary">Sign Up</button>
+                    <a href="https://app.openlaw.io/signup">Sign up for OpenLaw</a>
+                    </p>
                 </div>
             </div>
 
